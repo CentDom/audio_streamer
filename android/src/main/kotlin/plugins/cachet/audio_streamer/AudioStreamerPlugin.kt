73665ef5ba31ab13a81fa7273b34cd9c55fc6be6
Stanley Ndunwere <stanleyndunwere@gmail.com>
2023-05-09 15:07:45 +0100

    added streaming code

---
+++ android/src/main/kotlin/plugins/cachet/audio_streamer/AudioStreamerPlugin.kt
@@ -0,0 +1,217 @@
+package plugins.cachet.audio_streamer
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.os.Handler
+import android.os.Looper
+import android.os.Process
+import android.util.Log
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.floor
+
+/** AudioStreamerPlugin */
+class AudioStreamerPlugin : FlutterPlugin, RequestPermissionsResultListener,
+    EventChannel.StreamHandler, ActivityAware {
+
+    // / Constants
+    private val eventChannelName = "audio_streamer.eventChannel"
+
+    // / Method channel for returning the sample rate.
+    private val methodChannelName = "audio_streamer.methodChannel"
+    private var sampleRate = 44100 // standard value to initialize
+    private var bufferSize = 6400 * 2; // / Magical number!
+    private val maxAmplitude = 32767 // same as 2^15
+    private val logTag = "AudioStreamerPlugin"
+
+    // / Variables (i.e. will change value)
+    private var eventSink: EventSink? = null
+    private var recording = false
+
+    private var currentActivity: Activity? = null
+
+    private lateinit var audioRecord: AudioRecord;
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        val messenger = flutterPluginBinding.binaryMessenger
+        val eventChannel = EventChannel(messenger, eventChannelName)
+        eventChannel.setStreamHandler(this)
+        val methodChannel = MethodChannel(messenger, methodChannelName)
+        methodChannel.setMethodCallHandler { call, result ->
+            if (call.method == "getSampleRate") {
+                // Sample rate never changes, so return the given sample rate.
+                result.success(audioRecord?.getSampleRate())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        recording = false
+    }
+
+    override fun onDetachedFromActivity() {
+        currentActivity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        currentActivity = binding.activity
+        binding.addRequestPermissionsResultListener(this)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        currentActivity = binding.activity
+        binding.addRequestPermissionsResultListener(this)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        currentActivity = null
+    }
+
+    /**
+     * Called from Flutter, starts the stream and updates the sample rate using the argument.
+     */
+    override fun onListen(arguments: Any?, events: EventSink?) {
+        this.eventSink = events
+        recording = true
+        sampleRate = (arguments as Map<*, *>)["sampleRate"] as Int
+        bufferSize = (arguments as Map<*, *>)["bufferSize"] as Int
+        if (sampleRate < 4000 || sampleRate > 48000) {
+            events!!.error(
+                "SampleRateError",
+                "A sample rate of " + sampleRate + "Hz is not supported by Android.",
+                null
+            )
+            return
+        }
+        streamMicData()
+    }
+
+    /**
+     * Called from Flutter, which cancels the stream.
+     */
+    override fun onCancel(arguments: Any?) {
+        recording = false
+    }
+
+    /**
+     * Called by the plugin itself whenever it detects that permissions have not been granted.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ): Boolean {
+        val requestAudioPermissionCode = 200
+        when (requestCode) {
+            requestAudioPermissionCode -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) return true
+        }
+        return false
+    }
+
+    /**
+     * Starts recording and streaming audio data from the mic.
+     * Uses a buffer array of size 512. Whenever buffer is full, the content is sent to Flutter.
+     * Sets the sample rate as defined by [sampleRate].
+     *
+     * Source:
+     * https://www.newventuresoftware.com/blog/record-play-and-visualize-raw-audio-data-in-android
+     */
+    private fun streamMicData() {
+        Thread(
+            Runnable {
+                Process.setThreadPriority(Process.THREAD_PRIORITY_AUDIO)
+                val audioBuffer = ShortArray(bufferSize)
+                audioRecord = AudioRecord(
+                    MediaRecorder.AudioSource.DEFAULT,
+                    sampleRate,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize,
+                )
+                if (audioRecord.state != AudioRecord.STATE_INITIALIZED) {
+                    Log.e(logTag, "Audio Record can't initialize!")
+                    return@Runnable
+                }
+                /** Start recording loop */
+                audioRecord.startRecording()
+                var previousAudioBuffer = ArrayList<Double>()
+                var holderAudioBuffer = ArrayList<Double>()
+                val overlapPercent = 0.5
+                val overlap = 1 - overlapPercent
+                /** overlap is weird, 75 percent overlap means copying 75 percent
+                 * of the previous sample into the new one and incrementing by 100 -75%
+                 * until all values in the new array are copied */
+                while (recording) {
+                    /** Read data into buffer  */
+                    audioRecord.read(audioBuffer, 0, audioBuffer.size)
+                    Handler(Looper.getMainLooper()).post {
+                        // / Convert to list in order to send via EventChannel.
+                        val audioBufferList = ArrayList<Double>()
+                        for (impulse in audioBuffer) {
+                            val normalizedImpulse = impulse.toDouble() / maxAmplitude.toDouble()
+                            audioBufferList.add(normalizedImpulse)
+                        }
+
+                        if (overlap == 0.toDouble()) {
+                            eventSink!!.success(audioBufferList)
+                        } else {
+                            if (previousAudioBuffer.size == 0) {
+                                previousAudioBuffer.addAll(audioBufferList)
+                                eventSink!!.success(audioBufferList)
+                            } else {
+                                holderAudioBuffer.addAll(previousAudioBuffer)
+                                holderAudioBuffer.addAll(audioBufferList)
+
+                                var startIndex = (floor(overlap * audioBuffer.size)).toInt()
+                                val width = audioBuffer.size
+                                var endIndex = startIndex + width - 1
+
+                                while (startIndex < holderAudioBuffer.size) {
+                                    if (holderAudioBuffer.size - startIndex > audioBuffer.size) {
+                                        eventSink!!.success(
+                                            holderAudioBuffer.slice(
+                                                startIndex..endIndex
+                                            )
+                                        )
+                                        startIndex += (floor(overlap * audioBuffer.size)).toInt()
+                                        endIndex = startIndex + width - 1
+                                    } else if (holderAudioBuffer.size - startIndex == audioBuffer.size) {
+                                        eventSink!!.success(
+                                            holderAudioBuffer.slice(
+                                                startIndex..endIndex
+                                            )
+                                        )
+                                        startIndex = holderAudioBuffer.size
+                                        previousAudioBuffer.clear()
+                                    } else if (holderAudioBuffer.size - startIndex < audioBuffer.size) {
+                                        previousAudioBuffer = holderAudioBuffer.slice(
+                                            startIndex until holderAudioBuffer.size
+                                        ) as ArrayList<Double>
+                                        startIndex = holderAudioBuffer.size
+                                    }
+                                }
+
+                                holderAudioBuffer.clear()
+                            }
+                        }
+                    }
+                }
+                audioRecord.stop()
+                audioRecord.release()
+            },
+        ).start()
+    }
+}
---
+++ ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -0,0 +1,153 @@
+import AVFoundation
+import Flutter
+import UIKit
+
+public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+
+  private var eventSink: FlutterEventSink?
+  var engine = AVAudioEngine()
+  var audioData: [Float] = []
+  var recording = false
+  var preferredSampleRate: Int? = nil
+  var preferredBufferSize = 4096
+
+  // Register plugin
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let instance = SwiftAudioStreamerPlugin()
+
+    // Set flutter communication channel for emitting updates
+    let eventChannel = FlutterEventChannel.init(
+      name: "audio_streamer.eventChannel", binaryMessenger: registrar.messenger())
+    // Set flutter communication channel for receiving method calls
+    let methodChannel = FlutterMethodChannel.init(
+      name: "audio_streamer.methodChannel", binaryMessenger: registrar.messenger())
+    methodChannel.setMethodCallHandler { (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      if call.method == "getSampleRate" {
+        // Return sample rate that is currently being used, may differ from requested
+        result(Int(AVAudioSession.sharedInstance().sampleRate))
+      }
+    }
+    eventChannel.setStreamHandler(instance)
+    instance.setupNotifications()
+  }
+
+  private func setupNotifications() {
+    // Get the default notification center instance.
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(handleInterruption(notification:)),
+      name: AVAudioSession.interruptionNotification,
+      object: nil)
+  }
+
+  @objc func handleInterruption(notification: Notification) {
+    // If no eventSink to emit events to, do nothing (wait)
+    if eventSink == nil {
+      return
+    }
+
+    guard let userInfo = notification.userInfo,
+      let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
+      let type = AVAudioSession.InterruptionType(rawValue: typeValue)
+    else {
+      return
+    }
+
+    switch type {
+    case .began: ()
+    case .ended:
+      // An interruption ended. Resume playback, if appropriate.
+
+      guard let optionsValue = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else {
+        return
+      }
+      let options = AVAudioSession.InterruptionOptions(rawValue: optionsValue)
+      if options.contains(.shouldResume) {
+        startRecording(sampleRate: preferredSampleRate, bufferSize: UInt32(preferredBufferSize))
+      }
+
+    default:
+      eventSink!(
+        FlutterError(
+          code: "100", message: "Recording was interrupted",
+          details: "Another process interrupted recording."))
+    }
+  }
+
+  // Handle stream emitting (Swift => Flutter)
+  private func emitValues(values: [Float]) {
+
+    // If no eventSink to emit events to, do nothing (wait)
+    if eventSink == nil {
+      return
+    }
+    // Emit values count event to Flutter
+    eventSink!(values)
+  }
+
+  // Event Channel: On Stream Listen
+  public func onListen(
+    withArguments arguments: Any?,
+    eventSink: @escaping FlutterEventSink
+  ) -> FlutterError? {
+    self.eventSink = eventSink
+if let args = arguments as? [String: Any] {
+    if let preferredSampleRate = args["sampleRate"] as? Int,
+       let preferredBufferSize = args["bufferSize"] as? Int {
+        startRecording(sampleRate: preferredSampleRate, bufferSize: UInt32(preferredBufferSize))
+    } else {
+        // Handle the case where one or both arguments are missing or not of the expected type
+        print("Invalid or missing arguments (bufferSize or sampleRate")
+        startRecording(sampleRate: nil, bufferSize: nil)
+    }
+} else {
+    // Handle the case where the arguments parameter is not a dictionary
+    print("Arguments parameter is not a dictionary")
+    startRecording(sampleRate: nil, bufferSize: nil)
+}
+
+    return nil
+  }
+
+  // Event Channel: On Stream Cancelled
+  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    NotificationCenter.default.removeObserver(self)
+    eventSink = nil
+    engine.stop()
+    return nil
+  }
+
+  func startRecording(sampleRate: Int?, bufferSize: UInt32?) {
+    engine = AVAudioEngine()
+
+    do {
+      try AVAudioSession.sharedInstance().setCategory(
+        AVAudioSession.Category.playAndRecord, options: .mixWithOthers)
+      try AVAudioSession.sharedInstance().setActive(true)
+
+      if let sampleRateNotNull = sampleRate, let bufferSizeNotNull = bufferSize {
+        // Try to set sample rate
+        try AVAudioSession.sharedInstance().setPreferredSampleRate(Double(sampleRateNotNull))
+        try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(Double(bufferSizeNotNull) / Double(sampleRateNotNull))
+      }
+
+      let input = engine.inputNode
+      let bus = 0
+
+      input.installTap(onBus: bus, bufferSize: bufferSize ?? 4096, format: input.inputFormat(forBus: bus)) {
+        buffer, _ -> Void in
+        let samples = buffer.floatChannelData?[0]
+        // audio callback, samples in samples[0]...samples[buffer.frameLength-1]
+        let arr = Array(UnsafeBufferPointer(start: samples, count: Int(buffer.frameLength)))
+        self.emitValues(values: arr)
+      }
+
+      try engine.start()
+    } catch {
+      eventSink!(
+        FlutterError(
+          code: "100", message: "Unable to start audio session", details: error.localizedDescription
+        ))
+    }
+  }
+}
